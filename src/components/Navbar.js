3bb3bcbe9844a69e204fcbc367b5d74045969414
Lakshya <lakshyag9775@gmail.com>
2023-03-17 03:06:09 +0530

    Some bug fixed and minor changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/zestypratyush/my-porfolio"
+                href="https://github.com/Lakshya9775g/portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
